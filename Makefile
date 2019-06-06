2b2755d876ded99046b9a881034d781143802770
Pierlauro Sciarelli <sciarellip@gmail.com>
2019-06-06 14:16:12 +0200

    Timing r/w

--- Makefile
+++ Makefile
@@ -8,7 +8,7 @@ copy-python:
 	cp -r examples/python_vol build/
 
 main:
-	gcc -I/usr/include/python3.6m -I/usr/include/mpich -Isrc/c src/c/*.c examples/vol.c -lpython3.6m -lhdf5 -o build/vol
+	gcc -I/usr/include/python3.6m -I/usr/include/mpich -Isrc/c src/c/*.c examples/vol.c -lpython3.6m -lhdf5 -lm -lrt -o build/vol
 
 cpy:
 	gcc -I/usr/include/python3.6m -I/usr/include/mpich -Isrc/c src/c/*.c examples/c-py.c -lpython3.6m -lhdf5 -o build/c-py
--- examples/vol.c
+++ examples/vol.c
@@ -25,32 +25,60 @@ int main(int argc, char *argv[]){
 	fa.info = MPI_INFO_NULL;
 	H5Pset_vol(acc, vol_id, &fapl);
 
-	timing(write_read(1, acc));
+	write_read(1001, acc);
 
 	py_finalize();
 	return 0;
 }
 
 void write_read(int num_datasets, hid_t acc){
+	const int BUFFER_SIZE = 16;
+	
+	// Maximum number of digits to represent datasets indexes
+	int digits = (int)(floor (log10 (num_datasets))) + 2;
+	int names_size = 7 + digits;
+	char dataset_names[num_datasets][names_size];
+
+	int datasets[num_datasets][BUFFER_SIZE];
+	int read_buffers[num_datasets][BUFFER_SIZE];
+
+	hid_t dataset_ids[num_datasets];
+
+	// Initialize dataset names and fill datasets with their index
+	for(int i = 0; i < num_datasets; i++){
+		snprintf(dataset_names[i], names_size, "DATASET%d", i);
+		for(int j = 0; j < BUFFER_SIZE; j++){
+			datasets[i][j] = i;
+		}
+		printf("\n");
+	}
+
 	hid_t file_id = H5Fcreate("FILENAME.h5", H5F_ACC_TRUNC, H5P_DEFAULT, acc);
 	hid_t group_id = H5Gcreate(file_id, "/GROUP", H5P_DEFAULT, H5P_DEFAULT, H5P_DEFAULT);
 
-	const int BUFFER_SIZE = 16;
 	hsize_t dims[1] = {BUFFER_SIZE};
 	hid_t space = H5Screate_simple (1, dims, NULL);
 
-	hid_t dataset_id = H5Dcreate(group_id, "DATASET", H5T_NATIVE_INT, space, H5P_DEFAULT, H5P_DEFAULT, H5P_DEFAULT);
+	for(int i = 0; i < num_datasets; i++){
+		dataset_ids[i] = H5Dcreate(group_id, dataset_names[i], H5T_NATIVE_INT, space, H5P_DEFAULT, H5P_DEFAULT, H5P_DEFAULT);
+	}
 
-	int *buffer_write = (int *)calloc(sizeof(int), BUFFER_SIZE);
-	buffer_write[1] = 1;
-	buffer_write[2] = 2;
-	buffer_write[15] = 15;
-	H5Dwrite(dataset_id, H5T_NATIVE_INT, space, space, H5P_DEFAULT, buffer_write);
+timing(write,
+	for(int i = 0; i < num_datasets; i++){
+		H5Dwrite(dataset_ids[i], H5T_NATIVE_INT, space, space, H5P_DEFAULT, datasets[i]);
+	}
+)
 
-	int *buffer_read;
-	H5Dread(dataset_id, H5T_NATIVE_INT, space, space, H5P_DEFAULT, &buffer_read);
+timing(read,
+	for(int i = 0; i < num_datasets; i++){
+		H5Dread(dataset_ids[i], H5T_NATIVE_INT, space, space, H5P_DEFAULT, read_buffers[i]);
+	}
+)
 
-	H5Dclose(dataset_id);
+	for(int i = 0; i < num_datasets; i++){
+		H5Dclose(dataset_ids[i]);
+	}
+
 	H5Gclose(group_id);
 	H5Fclose(file_id);
 }
