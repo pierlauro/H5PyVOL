09c7dfc4c4329b725f34b82fd98369bcd4a1283f
Pierlauro Sciarelli <sciarellip@gmail.com>
2019-06-13 17:01:56 +0200

    Buffers allocation move on the heap

--- examples/vol.c
+++ examples/vol.c
@@ -32,23 +32,25 @@ int main(int argc, char *argv[]){
 }
 
 void write_read(int num_datasets, hid_t acc){
-	const int BUFFER_SIZE = 16;
+	const int BUFFER_SIZE = 100000;
 	
 	// Maximum number of digits to represent datasets indexes
 	int digits = (int)(floor (log10 (num_datasets))) + 2;
 	int names_size = 7 + digits;
+
 	char dataset_names[num_datasets][names_size];
 
-	int datasets[num_datasets][BUFFER_SIZE];
-	int read_buffers[num_datasets][BUFFER_SIZE];
-
-	hid_t dataset_ids[num_datasets];
+	int *datasets = calloc(num_datasets*BUFFER_SIZE, sizeof(int));
+	if(datasets == NULL){
+		perror("DATASETS NULL");
+		return;
+	}
 
 	// Initialize dataset names and fill datasets with their index
 	for(int i = 0; i < num_datasets; i++){
 		snprintf(dataset_names[i], names_size, "DATASET%d", i);
 		for(int j = 0; j < BUFFER_SIZE; j++){
-			datasets[i][j] = i;
+			datasets[i*BUFFER_SIZE + j] = i;
 		}
 	}
 
@@ -58,19 +60,25 @@ void write_read(int num_datasets, hid_t acc){
 	hsize_t dims[1] = {BUFFER_SIZE};
 	hid_t space = H5Screate_simple (1, dims, NULL);
 
+	hid_t dataset_ids[num_datasets];
 	for(int i = 0; i < num_datasets; i++){
 		dataset_ids[i] = H5Dcreate(group_id, dataset_names[i], H5T_NATIVE_INT, space, H5P_DEFAULT, H5P_DEFAULT, H5P_DEFAULT);
 	}
 
 timing(write,
 	for(int i = 0; i < num_datasets; i++){
-		H5Dwrite(dataset_ids[i], H5T_NATIVE_INT, space, space, H5P_DEFAULT, datasets[i]);
+		H5Dwrite(dataset_ids[i], H5T_NATIVE_INT, space, space, H5P_DEFAULT, &datasets[i*BUFFER_SIZE]);
 	}
 )
 
+	int *read_buffers = calloc(num_datasets * BUFFER_SIZE, sizeof(int));
+	if(read_buffers == NULL){
+		perror("read_buffers NULL");
+		return;
+	}
 timing(read,
 	for(int i = 0; i < num_datasets; i++){
-		H5Dread(dataset_ids[i], H5T_NATIVE_INT, space, space, H5P_DEFAULT, read_buffers[i]);
+		H5Dread(dataset_ids[i], H5T_NATIVE_INT, space, space, H5P_DEFAULT, &read_buffers[i*BUFFER_SIZE]);
 	}
 )
 
