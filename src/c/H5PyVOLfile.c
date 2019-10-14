1a36c34f7fe97acbb9755b1180a4acceaa6a5c3b
Pierlauro Sciarelli <sciarellip@gmail.com>
2019-10-14 14:12:16 +0000

    H5PyVOL supporting H5VL_FILE_GET_NAME

--- src/c/H5PyVOLfile.c
+++ src/c/H5PyVOLfile.c
@@ -1,4 +1,5 @@
 #include "H5PyVOL.h"
+#include <stdarg.h>
 
 void* H5VL_python_file_create(const char *name, unsigned flags, hid_t fcpl_id, hid_t fapl_id, hid_t dxpl_id, void **req){
 	char *method_name = "H5VL_python_file_create";
@@ -15,9 +16,22 @@ void* H5VL_python_file_open(const char *name, unsigned flags, hid_t fapl_id, hid
 }
 
 herr_t H5VL_python_file_get(void *obj, H5VL_file_get_t get_type, hid_t dxpl_id, void **req, va_list arguments){
-	char *method_name = "H5VL_python_file_get";
-	PyObject *ret = PyObject_CallMethod(obj, method_name, "llll", get_type, dxpl_id, req, arguments);
-	PyErr_Print();
+	if(get_type == H5VL_FILE_GET_NAME){
+		H5I_type_t type = (H5I_type_t)va_arg(arguments, int); /* enum work-around */
+		size_t size = va_arg(arguments, size_t);
+		char *name = va_arg(arguments, char *);
+		ssize_t *ret  = va_arg(arguments, ssize_t *);
+
+		PyObject *name_attribute = PyObject_GetAttr(obj, PyUnicode_FromString("name"));
+		char *tmp_name = PyBytes_AsString(PyUnicode_AsUTF8String(name_attribute));
+		size_t len = strlen(tmp_name);
+
+		if(name) {
+			memcpy(name, tmp_name, len + 1);
+		}
+
+		*ret = (ssize_t)len;
+	}
 	return 1;
 }
 
